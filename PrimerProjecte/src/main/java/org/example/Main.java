8f242fe4833b513be11cee9a1d576cb203ce1e57
mpallares <mpallares@iesebre.com>
2023-09-19 13:15:15 +0200

    He afegit el primer projecte java del curs

---
+++ PrimerProjecte/src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Main {
+
+//    int variable = 9;
+
+   public static void main(String[] args) {
+
+        System.out.println("Hola marc!");
+        System.out.println("Linea 2 maradona");
+        System.out.print("soes");
+        System.out.println("alumne");
+
+    }
+
+
+}
+
